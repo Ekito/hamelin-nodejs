82aa77a306b2dca7e9b7ad1ddeb92ec8a709458c
julienboulay <jboulay@ekito.fr>
2014-03-29 15:32:48 +0100

    Avoid broadcasting message => send to registered monitors. Code formating

--- server.js
+++ server.js
@@ -1,48 +1,105 @@
-var app = require('express')()
-  , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server)
-  , osc = require('osc-min')
-  , dgram = require('dgram');
+/**
+ * Init all modules and servers
+ */
+var app = require('express')();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+var osc = require('osc-min');
+var dgram = require('dgram');
 
-//var udp = dgram.createSocket("udp4");
-//
-//sendHeartbeat = function(myValue1, myValue2) {
-//  var buf;
-//  buf = osc.toBuffer({
-//    address: "/Fader",
-//    args: [
-//      myValue1, myValue2
-//    ]
-//  });
-//  return udp.send(buf, 0, buf.length, 8200, "192.168.5.118");
-//};
+var udp = dgram.createSocket("udp4");
 
-//setInterval(sendHeartbeat, 2000);
+var serverPort = process.env.PORT || 8080;
 
-
-server.listen(process.env.PORT || 8080);
-
-app.get('/:resource', function (req, res) {
-  res.sendfile(__dirname+'/web/'+req.params.resource);
+server.listen(serverPort, function() {
+	console.log((new Date()) + " Server is listening on port " + serverPort);
 });
 
-app.get('/js/:resource', function (req, res) {
-  res.sendfile(__dirname+'/web/js/'+req.params.resource);
+/**
+ * Init socket clients registries  
+ */
+//list of currently connected clients (users)
+var clients = [ ];
+
+//list of currently connected monitors
+var monitors = [ ];
+
+/**
+ * Init static resources path
+ */
+app.get('/:resource', function(req, res) {
+	res.sendfile(__dirname + '/web/' + req.params.resource);
 });
 
-app.get('/icons/:resource', function (req, res) {
-  res.sendfile(__dirname+'/web/icons/'+req.params.resource);
+app.get('/js/:resource', function(req, res) {
+	res.sendfile(__dirname + '/web/js/' + req.params.resource);
 });
 
+app.get('/icons/:resource', function(req, res) {
+	res.sendfile(__dirname + '/web/icons/' + req.params.resource);
+});
+
+/**
+ * Send OSC message
+ */
+
+sendOSC = function(myValue1, myValue2) {
+	var buf;
+	buf = osc.toBuffer({
+		address : "/Fader",
+		args : [ myValue1, myValue2 ]
+	});
+	return udp.send(buf, 0, buf.length, 8200, "192.168.5.118");
+};
+
+/**
+ * Socket.io connection management
+ */
 var id = 0;
-io.set('log level', 1);
-io.sockets.on('connection', function (socket) {
-    socket.on('getId', function (data){
-        socket.emit('id', id++);
-    });
+var monitor;
 
-    socket.on('deviceOrientation',function (data){
-//	sendHeartbeat(data.tiltLR, data.tiltFB);
-         socket.broadcast.emit('deviceOrientation', data);
-     });
+io.set('log level', 1);
+io.sockets.on('connection', function(socket) {
+
+	var index = clients.push(socket) - 1;
+	var isMonitor = false;
+	
+	
+	console.log(new Date() + "Connected on socket.io with ID: " + socket.id);
+
+	socket.on('getId', function(data) {
+		socket.emit('id', index);
+	});
+	
+	socket.on('registerMonitor', function(data) {
+		console.log(new Date() + "Receive register monitor: " + socket.id);
+		//Remove from the clients list and add it to the monitors
+		clients.splice(index, 1);
+		index = monitors.push(socket) - 1;
+		
+	});
+
+	socket.on('deviceOrientation', function(data) {
+		console.log(new Date() + "Receive device orientation: " + socket.id);
+		
+        for (var i = 0; i < monitors.length; i ++) { // broad cast on all monitors
+            if (monitors[i]) {
+            	monitors[i].emit('deviceOrientation', data);
+            }
+        }
+		
+		//Send OSC message to the server
+		//	sendOSC(data.tiltLR, data.tiltFB);
+		
+
+	});
+	
+	socket.on('disconnect', function(){
+		if (isMonitor) {
+			monitors.splice(index, 1);
+		}else {
+			clients.splice(index, 1);	
+		}
+		
+	});
 });
