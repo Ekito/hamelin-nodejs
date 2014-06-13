6c12e6489966be02af770582c318c656d42ffb2d
julienboulay <jboulay@ekito.fr>
2014-06-13 10:52:09 +0200

    Add leader/meneur selection

--- app/scripts/monitors/controllers/devicesMonitorCtrl.js
+++ app/scripts/monitors/controllers/devicesMonitorCtrl.js
@@ -1,9 +1,22 @@
-monitorsApp.controller('devicesMonitorCtrl', function($scope, monitorsSocket) {
+monitorsApp.controller('devicesMonitorCtrl', function($scope, $filter, monitorsSocket) {
 
 	$scope.connectedDevices = [];
+	$scope.leaderDevice = {};
 	
 	monitorsSocket.on('connectedDevices', function(data){
 		$scope.connectedDevices = data;
+		$scope.leaderDevice = $scope.connectedDevices.filter(function(device){
+			return device.leader;
+		})[0];
 	});
 	
+	$scope.selectLeader = function(device){
+		$scope.leaderDevice = device;
+		monitorsSocket.emit('leaderSelect', $scope.leaderDevice);
+	};
+	
+	$scope.unSelectLeader = function(){
+		monitorsSocket.emit('leaderUnselect', $scope.leaderDevice);
+		$scope.leaderDevice = {};
+	};
 });
